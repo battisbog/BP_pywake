e27c2653cc6192f8efcfd5b46e350cb2759e7103
battisbog <ishkiwashka@gmail.com>
2023-02-04 00:13:26 -0500

    created clipper object

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import numpy as np
+import random
 import xarray as xr
 import matplotlib.pyplot as plt
 from scipy.stats import exponweib
 
 # pywake packages
-
+from py_wake.wind_farm_models.engineering_models import PropagateDownwind
+from py_wake.deficit_models import TurboNOJDeficit
+from py_wake.rotor_avg_models import RotorCenter
+from py_wake.superposition_models import LinearSum
+from py_wake.turbulence_models import CrespoHernandez
+from py_wake.deflection_models.jimenez import JimenezWakeDeflection
 from py_wake.wind_turbines import WindTurbine
-from py_wake.wind_turbines.power_ct_functions import PowerCtFunction as powerCtFunction
+from py_wake.site._site import UniformWeibullSite
 from py_wake.wind_turbines.power_ct_functions import PowerCtTabular
+from py_wake.examples.data.hornsrev1 import Hornsrev1Site, V80, wt_x, wt_y, wt16_x, wt16_y
+from py_wake import NOJ
+from py_wake.wind_turbines.generic_wind_turbines import GenericWindTurbine
+
+
+
+
 ## create a wtg class which is an instance of a pywake wind turinbe item setting turbine hub height, rotor diameter, neame, and power curve function
 
-# (class) WindTurbine(name: Any, diameter: Any, hub_height: Any, powerCtFunction: Any, **windTurbineFunctions: Any)
+# clipper = GenericWindTurbine("clipper", 96,80, power_norm=10000, turbulence_intensity=.1)
 
 data = pd.read_csv('wind_data_merged.csv')
+wind_speed = data['WindSpeed (m/s)']
+wind_power = data['Wind Power (W)']
+ct = [.8]*len(wind_speed)
+# print(ct[0:10])
 
-# for i in range(100):
-#     wt = WindTurbine(name="wt", diameter=96, hub_height=80, powerCtFunction=powerCtFunction)
-#     plt.plot(wt.power(ws, rho=r)/1000, label=f'Air density: {r}')
-# plt.ylabel('Power [kW]')
-# plt.xlabel('Wind speed [m/s]')
-# plt.legend()
-print(data)
+clipper = WindTurbine(name='clipper',
+                    diameter=96,
+                    hub_height=80,
+                    powerCtFunction=PowerCtTabular(wind_speed,wind_power,'kW',ct))
 
+
+windTurbines = clipper()
+site = Hornsrev1Site()
+noj = NOJ(site,windTurbines)
+simulationResult = noj(wt16_x,wt16_y)
+simulationResult.aep()
+print(simulationResult.aep().sum())
+
+
+
+
+
+# plt.plot(wind_speed,power)
---
+++ scratch.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+x = [0,1,2,3,4,5]
+y = [0,1,2,3,4,5]
+plt.plot(x,y)
