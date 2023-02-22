4b59edb21e90b1a733b6d977284673690308f37a
battisbog <ishkiwashka@gmail.com>
2023-02-22 15:33:51 -0500

    non

--- example.py
+++ example.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from py_wake.examples.data.hornsrev1 import Hornsrev1Site
 from py_wake.examples.data.hornsrev1 import HornsrevV80
-from py_wake.deficit_models.floris import FLORIS
+# from py_wake.deficit_models.floris import FLORIS
+from py_wake.deficit_models.noj import NOJDeficit, NOJ
 from py_wake.wind_farm_models.wind_farm_model import WindFarmModel
 
 # Define the site (Horns Rev 1 offshore wind farm)
@@ -12,7 +13,7 @@ site = Hornsrev1Site()
 v80 = HornsrevV80()
 
 # Define the wind farm (single Vestas V80 turbine at origin, FLORIS wake model)
-wf_model = WindFarmModel(site, [v80], FLORIS())
+wf_model = WindFarmModel(site, [v80])
 
 # Define the wind speeds at which to calculate power output
 wind_speeds = np.arange(4, 26, 2)
--- main.py
+++ main.py
@@ -1,104 +1,28 @@
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-import pandas as pd
-import numpy as np
-import random
-import xarray as xr
 import matplotlib.pyplot as plt
-from scipy.stats import exponweib
-
-# pywake packages
-from py_wake.wind_farm_models.engineering_models import PropagateDownwind
-from py_wake.deficit_models import TurboNOJDeficit
-from py_wake.rotor_avg_models import RotorCenter
-from py_wake.superposition_models import LinearSum
-from py_wake.turbulence_models import CrespoHernandez
-from py_wake.deflection_models.jimenez import JimenezWakeDeflection
-from py_wake.wind_turbines import WindTurbine
-from py_wake.site._site import UniformWeibullSite
-from py_wake.wind_turbines.power_ct_functions import PowerCtTabular
-from py_wake.examples.data.hornsrev1 import Hornsrev1Site, V80, wt_x, wt_y, wt16_x, wt16_y
+import numpy as np
+from py_wake.examples.data.hornsrev1 import Hornsrev1Site
+from py_wake.examples.data.hornsrev1 import HornsrevV80
 from py_wake import NOJ
-from py_wake.wind_turbines.generic_wind_turbines import GenericWindTurbine
-from py_wake import BastankhahGaussian
+from py_wake.wind_farm_models.wind_farm_model import WindFarmModel
 
-
-
-
-## create a wtg class which is an instance of a pywake wind turinbe item setting turbine hub height, rotor diameter, neame, and power curve function
-
-# clipper = GenericWindTurbine("clipper", 96,80, power_norm=10000, turbulence_intensity=.1)
-
-data = pd.read_csv('wind_data_merged.csv')
-wind_speed = data['WindSpeed (m/s)']
-wind_speed = wind_speed.iloc[1:36]
-wind_power = data['Wind Power (W)']
-wind_power = wind_power.iloc[1:36]
-
-# wind_speed = data(index=range(0,36), columns=['WindSpeed (m/s)'])
-# wind_power = data(index=range(0,36), columns=['Wind Power (W)'])
-
-ct = [.59]*len(wind_speed)
-# print(ct[0:10])
-
-clipper = WindTurbine(name='clipper',
-                    diameter=96,
-                    hub_height=80,
-                    powerCtFunction=PowerCtTabular(wind_speed,wind_power,'kW',ct))
-
-
-
-# windTurbines = clipper()
+# Define the site (Horns Rev 1 offshore wind farm)
 site = Hornsrev1Site()
-noj = NOJ(site,clipper)
-simulationResult = noj(wt16_x,wt16_y)
-aep = simulationResult.aep()
-total_aep = simulationResult.aep().sum()
-total_aep = total_aep.item()
-fm = simulationResult.flow_map()
-# power = power.power_xylk()
 
-plt.figure()
-fm.plot(fm.power_xylk().sum(['wd', 'ws']) * 1e-3, "Power [kW]")
+# Define the wind turbines (Vestas V80)
+turbines = HornsrevV80()
+
+# Define the wind farm (multiple Vestas V80 turbines distributed on the site, NOJ wake model)
+wind_turbines = turbines.create_layout(site, 3, 3)
+wf_model = WindFarmModel(site, wind_turbines, NOJ())
+
+# Define the wind speeds at which to calculate power output
+wind_speeds = np.arange(4, 26, 2)
+
+# Calculate the power output at each wind speed for the wind farm
+p = wf_model.power(wind_speeds)
+
+# Plot the power output against the wind speed
+plt.plot(wind_speeds, p / 1000, '-o')
+plt.xlabel('Wind speed (m/s)')
+plt.ylabel('Power output (kW)')
 plt.show()
-print("works")
-
-
-wf_model = BastankhahGaussian(site, clipper)
-sim_res = wf_model(type=0,   # Wind turbine types
-                   wd=None,  # Wind direction
-                   ws=wind_speed
-                    )
-
-# print(total_aep)
-# print("Total annual energy production = "+str(total_aep) + " GWh")
-
-# power = simulationResult
-
-# plt.figure()
-# aep.sum(['wt','wd']).plot()
-# plt.xlabel("Wind speed [m/s]")
-# plt.ylabel("AEP [GWh]")
-# plt.title('AEP vs wind speed')
-# plt.show()
-
-
-# plt.figure()
-# aep.sum(['wt','wd']).plot()
-# plt.xlabel("Wind speed [m/s]")
-# plt.ylabel("AEP [GWh]")
-# plt.title('AEP vs wind speed')
-# plt.show()
-
-# plt.figure()
-# aep = simulationResult.aep()
-# c =plt.scatter(wt16_x, wt16_y, c=aep.sum(['wd','ws']))
-# plt.colorbar(c, label='AEP [GWh]')
-# plt.title('AEP of each turbine')
-# plt.xlabel('x [m]')
-# plt.ylabel('[m]')
-# plt.show()
-
-
-
-# plt.plot(wind_speed,power)
