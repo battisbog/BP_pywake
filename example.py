db6678e622447b4f588c2ceabcecf95693e2fa2a
battisbog <ishkiwashka@gmail.com>
2023-03-02 09:14:19 -0500

    -

--- example.py
+++ example.py
@@ -6,16 +6,12 @@ from py_wake.examples.data.hornsrev1 import HornsrevV80
 from py_wake.deficit_models.noj import NOJDeficit, NOJ
 from py_wake.wind_farm_models.wind_farm_model import WindFarmModel
 
-# Define the site (Horns Rev 1 offshore wind farm)
+# Define the site and turbine
 site = Hornsrev1Site()
-
-# Define the wind turbine (Vestas V80)
 v80 = HornsrevV80()
 
 # Define the wind farm (single Vestas V80 turbine at origin, FLORIS wake model)
 wf_model = WindFarmModel(site, [v80])
-
-# Define the wind speeds at which to calculate power output
 wind_speeds = np.arange(4, 26, 2)
 
 # Calculate the power output at each wind speed for the single turbine
