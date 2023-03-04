317991cd85705e92c857aa962148f48888012442
battisbog <ishkiwashka@gmail.com>
2023-03-04 14:41:09 -0500

    new site

--- main.py
+++ main.py
@@ -9,20 +9,7 @@ from py_wake.wind_farm_models.wind_farm_model import WindFarmModel
 site = Hornsrev1Site()
 
 # Define the wind turbines (Vestas V80)
-turbines = HornsrevV80()
-
-# Define the wind farm (multiple Vestas V80 turbines distributed on the site, NOJ wake model)
-wind_turbines = turbines.create_layout(site, 3, 3)
-wf_model = WindFarmModel(site, wind_turbines, NOJ())
-
-# Define the wind speeds at which to calculate power output
-wind_speeds = np.arange(4, 26, 2)
-
-# Calculate the power output at each wind speed for the wind farm
-p = wf_model.power(wind_speeds)
-
-# Plot the power output against the wind speed
-plt.plot(wind_speeds, p / 1000, '-o')
-plt.xlabel('Wind speed (m/s)')
-plt.ylabel('Power output (kW)')
-plt.show()
+clipper = WindTurbine(name='clipper',
+                    diameter=96,
+                    hub_height=80,
+                    powerCtFunction=PowerCtTabular(FO601_ws,FO601_pw,'kW',ct))
