01721ff6cc76b9e2b9fbf3c3d2392c60bd29a947
nvs5574 <nvs5574@psu.edu>
2023-03-29 14:07:18 -0400

    added code for wind rose

--- nic.py
+++ nic.py
@@ -78,3 +78,9 @@ st.pyplot(fig=fig)
 
 # plt.plot(wind_rose)
 
+import plotly.express as px
+df = px.data.wind()
+fig = px.bar_polar(df, r="frequency", theta="direction",
+                   color="strength", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig.show()
